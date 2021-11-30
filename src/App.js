4f816b4477d5c7dc690a53320c3a5b70ecea5179
Sharan AJ <sharanajs2001@gmail.com>
2021-11-30 23:35:33 +0530

    tailwindcss integrated successfully and working perfectly

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gray-600">
       <header className="App-header">
-        <h1>Amzon Clone with React</h1>
+        <h1 className="text-center text-yellow-400 py-12 text-4xl">Amazon Clone with React</h1>
       </header>
     </div>
   );
